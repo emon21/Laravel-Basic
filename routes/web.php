136669badc03e175020b60f589520b8cbc8cf6d5
emon21 <emonbd66@gmail.com>
2023-12-04 23:15:57 +0600

    Breeze Package Authentication install

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\frontend\FrontendController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,14 +14,23 @@ use App\Http\Controllers\frontend\FrontendController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', function () {
-
-     echo'<h2>Well Come To Laravel</h2>';
-
+    return view('welcome');
 });
 
-Route::get('test',[FrontendController::class,'index']);
+
+Route::get('laravel',function(){
+    return view('laravel');
+})->middleware('auth');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
